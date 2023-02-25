595d12f3a819a0d1c3b295c3566903e983f001c6
evilpeach <kanisorn.peach@gmail.com>
2023-02-25 17:09:44 +0700

    add signAmino

--- wallets/terra-extension/src/extension/signer.ts
+++ wallets/terra-extension/src/extension/signer.ts
@@ -1,10 +1,14 @@
 import {
-  OfflineDirectSigner,
   AccountData,
   DirectSignResponse,
   encodePubkey,
 } from '@cosmjs/proto-signing';
-import { StdSignature } from '@cosmjs/amino';
+import {
+  OfflineAminoSigner,
+  AminoSignResponse,
+  StdSignDoc,
+  StdSignature,
+} from '@cosmjs/amino';
 import { WalletAccount } from '@cosmos-kit/core';
 import { TerraExtension } from './extension';
 import {
@@ -12,7 +16,11 @@ import {
   AuthInfo,
   SignerInfo,
 } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
-import { Fee as TerraFee, Msg as TerraMsg } from '@terra-money/feather.js';
+import {
+  Fee as TerraFee,
+  Msg as TerraMsg,
+  SignMode,
+} from '@terra-money/feather.js';
 
 export interface SignDoc {
   bodyBytes: Uint8Array;
@@ -21,12 +29,12 @@ export interface SignDoc {
   accountNumber: Long;
 }
 
-export class OfflineSigner implements OfflineDirectSigner {
-  private client: TerraExtension;
+export class OfflineSigner implements OfflineAminoSigner {
+  private extension: TerraExtension;
   accountInfo: WalletAccount;
 
-  constructor(client: TerraExtension, accountInfo: WalletAccount) {
-    this.client = client;
+  constructor(extension: TerraExtension, accountInfo: WalletAccount) {
+    this.extension = extension;
     this.accountInfo = accountInfo;
   }
 
@@ -40,7 +48,52 @@ export class OfflineSigner implements OfflineDirectSigner {
     ];
   }
 
-  async signDirect(
+  async signAmino(
+    signerAddress: string,
+    signDoc: StdSignDoc
+  ): Promise<AminoSignResponse> {
+    console.log('signAmino', signerAddress, signDoc);
+    const signDocFee = signDoc.fee;
+    const feeAmount = signDocFee.amount[0].amount + signDocFee.amount[0].denom;
+
+    const fakeMsgs = signDoc.msgs.map((msg) =>
+      TerraMsg.fromAmino(msg as TerraMsg.Amino)
+    );
+
+    const signResponse = await this.extension.sign({
+      chainID: signDoc.chain_id,
+      msgs: fakeMsgs as any,
+      // remark
+      // Not sure how to request signMode to station wallet
+      // station keep using SIGN_MODE_DIRECT
+      signMode: SignMode.SIGN_MODE_LEGACY_AMINO_JSON,
+      fee: new TerraFee(
+        parseInt(signDocFee.gas),
+        feeAmount,
+        signDocFee.payer,
+        signDocFee.granter
+      ),
+      memo: signDoc.memo,
+    } as any);
+
+    console.log('signResponse xx', signResponse);
+
+    const signature: StdSignature = {
+      pub_key: (signResponse.payload.result.auth_info.signer_infos[0]
+        .public_key as any).key,
+      signature: signResponse.payload.result.signatures[0],
+    };
+
+    console.log('signature', signature);
+
+    return {
+      signed: signDoc,
+      signature,
+    };
+  }
+
+  // To force `cosmos-kit` use `signAmino` instead of `signDirect`
+  async signDirectKKK(
     _signerAddress: string,
     _signDoc: SignDoc
   ): Promise<DirectSignResponse> {
@@ -59,7 +112,7 @@ export class OfflineSigner implements OfflineDirectSigner {
 
     const chainID = _signDoc.chainId;
 
-    const signResponse = await this.client.sign({
+    const signResponse = await this.extension.sign({
       chainID,
       msgs: txbody.messages.map((msg) => TerraMsg.fromProto(msg)),
       fee,
